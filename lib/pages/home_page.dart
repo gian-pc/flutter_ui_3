f1a17808a3db2c86c9a2f89e38380c6843075475
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-06 17:57:51 -0500

    Carousel

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'package:fl_chart/fl_chart.dart';
-
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,7 +9,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   int touchedIndex = -1;
 
   @override
@@ -43,55 +40,89 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: Drawer(),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "My Files",
-                    style: TextStyle(
-                      color: Color(0xff565656),
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.bold
-                    ),
+      // body: SafeArea(
+      //   child: SingleChildScrollView(
+      //     padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
+      //     child: Column(
+      //       children: [
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //           children: [
+      //             Text(
+      //               "My Files",
+      //               style: TextStyle(
+      //                 color: Color(0xff565656),
+      //                 fontSize: 26.0,
+      //                 fontWeight: FontWeight.bold
+      //               ),
+      //             ),
+      //             Icon(
+      //               Icons.more_horiz,
+      //               color: Color(0xff7f7f7f),
+      //             )
+      //           ],
+      //         ),
+      //         AspectRatio(
+      //           aspectRatio: 1.4,
+      //           child: PieChart(
+      //             PieChartData(
+      //                 pieTouchData:
+      //                 PieTouchData(touchCallback: (FlTouchEvent event, pieTouchResponse) {
+      //                   setState(() {
+      //                     if (!event.isInterestedForInteractions ||
+      //                         pieTouchResponse == null ||
+      //                         pieTouchResponse.touchedSection == null) {
+      //                       touchedIndex = -1;
+      //                       return;
+      //                     }
+      //                     touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
+      //                   });
+      //                 }),
+      //                 borderData: FlBorderData(
+      //                   show: false,
+      //                 ),
+      //                 sectionsSpace: 0,
+      //                 centerSpaceRadius: 49,
+      //                 sections: showingSections()),
+      //           ),
+      //         ),
+      //         Text("sdsd")
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              height: 240.0,
+              //color: Colors.purpleAccent,
+              child: PageView.builder(
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                  controller: PageController(
+                    initialPage: 4,
+                    viewportFraction: 0.5,
                   ),
-                  Icon(
-                    Icons.more_horiz,
-                    color: Color(0xff7f7f7f),
-                  )
-                ],
-              ),
-              AspectRatio(
-                aspectRatio: 1.4,
-                child: PieChart(
-                  PieChartData(
-                      pieTouchData:
-                      PieTouchData(touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                        setState(() {
-                          if (!event.isInterestedForInteractions ||
-                              pieTouchResponse == null ||
-                              pieTouchResponse.touchedSection == null) {
-                            touchedIndex = -1;
-                            return;
-                          }
-                          touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
-                        });
-                      }),
-                      borderData: FlBorderData(
-                        show: false,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.greenAccent,
+                        borderRadius: BorderRadius.circular(16.0)
                       ),
-                      sectionsSpace: 0,
-                      centerSpaceRadius: 49,
-                      sections: showingSections()),
-                ),
-              ),
-              Text("sdsd")
-            ],
-          ),
+                      child: Center(
+                        child: Text(
+                          index.toString(),
+                          style: TextStyle(fontSize: 50),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+
+          ],
         ),
       ),
     );
@@ -110,7 +141,9 @@ class _HomePageState extends State<HomePage> {
             title: '40%',
             radius: radius,
             titleStyle: TextStyle(
-                fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
           );
         case 1:
           return PieChartSectionData(
@@ -119,7 +152,9 @@ class _HomePageState extends State<HomePage> {
             title: '30%',
             radius: radius,
             titleStyle: TextStyle(
-                fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
           );
         case 2:
           return PieChartSectionData(
@@ -128,7 +163,9 @@ class _HomePageState extends State<HomePage> {
             title: '15%',
             radius: radius,
             titleStyle: TextStyle(
-                fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
           );
         case 3:
           return PieChartSectionData(
@@ -137,7 +174,9 @@ class _HomePageState extends State<HomePage> {
             title: '15%',
             radius: radius,
             titleStyle: TextStyle(
-                fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
           );
         default:
           throw Error();
@@ -145,6 +184,3 @@ class _HomePageState extends State<HomePage> {
     });
   }
 }
-
-
-
