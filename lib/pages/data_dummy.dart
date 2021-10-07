1f952917d4e3d31a043ad476773dc2624ff8c684
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-06 22:37:12 -0500

    UI_3 Finish

---
+++ lib/pages/data_dummy.dart
@@ -0,0 +1,48 @@
+
+
+import 'package:flutter/material.dart';
+
+Map<int,dynamic>miDataMap={
+  1:{
+    "backgroundColor": Color(0xffC2F4E9),
+    "icon":Icons.folder,
+    "iconColor":Color(0xff1ED2A4),
+    "title":"Photos",
+    "number":"564",
+  },
+  2:{
+    "backgroundColor": Color(0xffFCF0BE),
+    "icon":Icons.photo,
+    "iconColor":Color(0xffF4B900),
+    "title":"Photos",
+    "number":"100",
+  },
+  3:{
+    "backgroundColor": Color(0xffCED1FF),
+    "icon":Icons.music_note,
+    "iconColor":Color(0xff5C48F2),
+    "title":"Music",
+    "number":"243",
+  },
+  4:{
+    "backgroundColor": Color(0xffC2F4E9),
+    "icon":Icons.folder,
+    "iconColor":Color(0xff1ED2A4),
+    "title":"Photos",
+    "number":"564",
+  },
+  5:{
+    "backgroundColor": Color(0xffFCF0BE),
+    "icon":Icons.photo,
+    "iconColor":Color(0xffF4B900),
+    "title":"Photos",
+    "number":"100",
+  },
+  6:{
+    "backgroundColor": Color(0xffCED1FF),
+    "icon":Icons.music_note,
+    "iconColor":Color(0xff5C48F2),
+    "title":"Music",
+    "number":"243",
+  },
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,9 @@
+import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter_codigo3_ui_3/pages/data_dummy.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -41,129 +42,125 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: Drawer(),
-      // body: SafeArea(
-      //   child: SingleChildScrollView(
-      //     padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
-      //     child: Column(
-      //       children: [
-      //         Row(
-      //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //           children: [
-      //             Text(
-      //               "My Files",
-      //               style: TextStyle(
-      //                 color: Color(0xff565656),
-      //                 fontSize: 26.0,
-      //                 fontWeight: FontWeight.bold
-      //               ),
-      //             ),
-      //             Icon(
-      //               Icons.more_horiz,
-      //               color: Color(0xff7f7f7f),
-      //             )
-      //           ],
-      //         ),
-      //         AspectRatio(
-      //           aspectRatio: 1.4,
-      //           child: PieChart(
-      //             PieChartData(
-      //                 pieTouchData:
-      //                 PieTouchData(touchCallback: (FlTouchEvent event, pieTouchResponse) {
-      //                   setState(() {
-      //                     if (!event.isInterestedForInteractions ||
-      //                         pieTouchResponse == null ||
-      //                         pieTouchResponse.touchedSection == null) {
-      //                       touchedIndex = -1;
-      //                       return;
-      //                     }
-      //                     touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
-      //                   });
-      //                 }),
-      //                 borderData: FlBorderData(
-      //                   show: false,
-      //                 ),
-      //                 sectionsSpace: 0,
-      //                 centerSpaceRadius: 49,
-      //                 sections: showingSections()),
-      //           ),
-      //         ),
-      //         Text("sdsd")
-      //       ],
-      //     ),
-      //   ),
-      // ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              height: 240.0,
-              //color: Colors.purpleAccent,
-              child: PageView.builder(
-                  itemCount: 10,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Color(0xff00D399),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "My Files",
+                    style: TextStyle(
+                        color: Color(0xff565656),
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Icon(
+                    Icons.more_horiz,
+                    color: Color(0xff7f7f7f),
+                  )
+                ],
+              ),
+              AspectRatio(
+                aspectRatio: 1.4,
+                child: PieChart(
+                  PieChartData(
+                      pieTouchData: PieTouchData(touchCallback:
+                          (FlTouchEvent event, pieTouchResponse) {
+                        setState(() {
+                          if (!event.isInterestedForInteractions ||
+                              pieTouchResponse == null ||
+                              pieTouchResponse.touchedSection == null) {
+                            touchedIndex = -1;
+                            return;
+                          }
+                          touchedIndex = pieTouchResponse
+                              .touchedSection!.touchedSectionIndex;
+                        });
+                      }),
+                      borderData: FlBorderData(
+                        show: false,
+                      ),
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 49,
+                      sections: showingSections()),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                height: 240.0,
+                //color: Colors.purpleAccent,
+                child: PageView.builder(
+                  itemCount: miDataMap.length,
                   scrollDirection: Axis.horizontal,
                   controller: PageController(
                     initialPage: 4,
                     viewportFraction: 0.5,
                   ),
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 18.0, horizontal: 12.0),
-                      margin: EdgeInsets.symmetric(horizontal: 10.0),
-                      decoration: BoxDecoration(
-                          color: Color(0xffC2F4E9),
-                          borderRadius: BorderRadius.circular(16.0)),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Icon(
-                            Icons.folder,
-                            color: Color(0xff1ED2A4),
-                            size: 50,
-                          ),
-                          Expanded(child: Container()),
-                          Text(
-                            "Photos",
-                            style: TextStyle(
-                              fontSize: 26.0,
-                              color: Colors.black87.withOpacity(0.65),
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5.0,
-                          ),
-                          Text(
-                            "564 Items",
-                            style:
-                                TextStyle(color: Colors.black54, fontSize: 16),
-                          ),
-                          SizedBox(
-                            height: 5.0,
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.lock,
-                                color: Color(0xff1ED2A4),
-                                size: 19,
-                              ),
-                              Text(
-                                "Private Folder",
-                                style: TextStyle(
-                                    fontSize: 12.0, color: Colors.black54),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
+                  itemBuilder: (BuildContext context, int i) {
+                    return ItemCarouselWidget(
+                      index: i + 1,
                     );
-                  }),
-            ),
-          ],
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Latest Files",
+                    style: TextStyle(
+                        color: Color(0xff565656),
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Icon(
+                    Icons.more_horiz,
+                    color: Color(0xff7f7f7f),
+                  )
+                ],
+              ),
+              ListView.builder(
+                itemCount: 4,
+                primary: false,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index){
+                  return ListTile(
+                    leading: Container(
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xffCED1FF),
+                        borderRadius: BorderRadius.circular(10.0)
+                      ),
+                      child: Icon(Icons.file_copy, color: Color(0xff5C48F2),),
+                    ),
+                    title: Text("Podscat with Brenda Evans"),
+                    subtitle: Text("32Mb March 12, 2021"),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
+      // body: SingleChildScrollView(
+      //   child: Column(
+      //     children: [
+      //
+      //     ],
+      //   ),
+      // ),
     );
   }
 
@@ -221,5 +218,63 @@ class _HomePageState extends State<HomePage> {
           throw Error();
       }
     });
+  }
+}
+
+class ItemCarouselWidget extends StatelessWidget {
+  int index;
+
+  ItemCarouselWidget({required this.index});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 18.0, horizontal: 12.0),
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      decoration: BoxDecoration(
+          color: miDataMap[index]["backgroundColor"],
+          borderRadius: BorderRadius.circular(16.0)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            miDataMap[index]["icon"],
+            color: miDataMap[index]["iconColor"],
+            size: 50,
+          ),
+          Expanded(child: Container()),
+          Text(
+            miDataMap[index]["title"],
+            style: TextStyle(
+              fontSize: 26.0,
+              color: Colors.black87.withOpacity(0.65),
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            "${miDataMap[index]["number"]} Items",
+            style: TextStyle(color: Colors.black54, fontSize: 16),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.lock,
+                color: miDataMap[index]["iconColor"],
+                size: 19,
+              ),
+              Text(
+                "Private Folder",
+                style: TextStyle(fontSize: 12.0, color: Colors.black54),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
